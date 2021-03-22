25ae72109955eb4d914a8f5f264e4b0b3c05091c
patakapata <xxthunderwavexx@gmail.com>
2021-03-22 18:13:45 +0900

    Initial Commit

---
+++ src/main/java/com/theboss/kzeaddonfabric/KZEAddonFabric.java
@@ -0,0 +1,13 @@
+package com.theboss.kzeaddonfabric;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class KZEAddonFabric implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
